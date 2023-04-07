24ad9130d3497b46ff53b0daf6d34eee84714307
plzdontcry <naraphat.naem@gmail.com>
2023-04-07 14:14:02 +0700

    feat: poc book list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const Greeting = () => <h1>helo</h1>;
+
+import "./index.css";
+
+export const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+export const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+export const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/41+j7f1fVKL._SY344_BO1,204,203,200_.jpg"
+      alt=""
+    />
+  );
+};
+
+export const Title = () => {
+  return <h2>Saved: A War Reporter's Mission to Make It Home</h2>;
+};
+
+export const Author = () => {
+  return <h2>Benjamin Hall</h2>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<BookList />);
